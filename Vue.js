3b23656d2b6715eb9f2484c29ed178b15294c7f8
AceVentura <alexpapinutti@gmail.com>
2018-10-29 13:40:25 +0100

    Completed v.1

--- Vue.js
+++ Vue.js
@@ -2,62 +2,61 @@ var vm = new Vue({
     el: "#app",
     data: {
         allInfo: [],
-        
+        search: '',
         link: "https://api.myjson.com/bins/1h3vb3",
     },
     created: function () {
         this.loadData(this.link);
-//        this.getCard();
         
+        
+
+    },
+    computed: {
+        filterBooks: function(){
+            return this.allInfo.filter((data) =>{
+                var mySearch, myBooks;
+                mySearch = this.search.toLowerCase();
+                myBooks = data.titulo.toLowerCase();
+                return myBooks.match(this.search);
+            });
+        }
     },
     methods: {
 
         loadData: function (link) {
-                var fetchConfig =
-                    fetch(link, {
-                        method: "GET",
-                    })
+            var fetchConfig =
+                fetch(link, {
+                    method: "GET",
+                })
 
-                    .then(function (response) {
-                        return response.json();
+                .then(function (response) {
+                    return response.json();
 
-                    })
-                    .then(function (data) {
-                        vm.getInformation(data);
-                        vm.showPicture();
-                    })
-            .catch(error => console.log(error));
+                })
+                .then(function (data) {
+                    var books;
+                    vm.books = data.books;
+                    vm.getInformation(data);
+                    
+                })
+                .catch(error => console.log(error));
 
-    },
-    getInformation: function (data) {
-        var books;
-        var newInfo = [];
-        books = data.books;
-    
-        for (var i = 0; i < books.length; i++) {
-            newInfo.push(books[i]);
-            vm.allInfo= newInfo;
+        },
+        getInformation: function (data) {
+            var books;
+            var newInfo = [];
+            books = data.books;
+            
+            for (var i = 0; i < books.length; i++) {
+                newInfo.push(books[i]);
+                vm.allInfo = newInfo;
+
+            }
+        },
+            
+        
      
-        }
-        console.log(vm.allInfo);
-        
-    },
-    showPicture: function(){
-        var button;
-        console.log("hey");
-    }
-//    getCard: function() {
-//        var card;
-//        card = this.document.querySelector('.card');
-//        card.addEventListener("mouseover", function(){
-//            card.classList.toggle('is-flipped');
-//        });
-//        
-//        
-//           
-//    },
-        
 
-    
+
     }
 });
